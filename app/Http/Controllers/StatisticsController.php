454a1c582740497212432910c68763090e147462
nguyenphuocdai <hoanghoang360@gmail.com>
2017-11-28 13:13:11 +0700

    update

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -7,6 +7,7 @@ use DB;
 use App\customers;
 use DateTime;
 use App\products;
+use Validator;
 use Illuminate\Support\Facades\Auth;
 class StatisticsController extends Controller
 {
@@ -20,13 +21,26 @@ class StatisticsController extends Controller
          else
             return redirect()->back()->with(['flash_level'=>'danger','flash_message'=>'Bạn không có quyền sử dụng chức năng này !']);
     }
-    public function postChooseDay(Request $r)
+    public function postChooseDay(Request $request)
     {
-    	$tamp = DB::table('orders_detail')->select('product_id', DB::raw('sum(quantity) as total'))->whereBetween('date_signed',[$r->txtStartDate,$r->txtEndDate])->groupBy('product_id')->orderBy('total','desc')->get();
-        $sd= $r->txtStartDate;
-        $ed = $r->txtEndDate;
+        $validator = Validator::make($request->all(),[
+            'txtStartDate' => 'required|date',
+            'txtEndDate' => 'required|date|after_or_equal:txtStartDate',
+        ],[
+            'txtStartDate.required' => 'Bạn chưa chọn ngày bắt đầu.',
+            'txtEndDate.required' => 'Bạn chưa chọn ngày bắt đầu.',
+            'txtEndDate.after_or_equal' => 'Ngày kết thúc phải sau ngày bắt đầu.'
+        ]);
+        if($validator->passes()){
+    	$tamp = DB::table('orders_detail')->select('product_id', DB::raw('sum(quantity) as total'))->whereBetween('date_signed',[$request->txtStartDate,$request->txtEndDate])->groupBy('product_id')->orderBy('total','desc')->get();
+        $sd= $request->txtStartDate;
+        $ed = $request->txtEndDate;
     	
     	return view('admin.statistics.chooseday',compact('tamp','sd','ed'));
+        }
+        else{
+            return redirect()->back()->with(['flash_level'=>'danger','flash_message'=>'Ngày bắt đầu và ngày kết thúc là bắt buộc chọn. Ngày kết thúc phải sau ngày bắt đầu']);
+        }
     }
     public function getCustomer(){
         $data = DB::table('customers')->get();
